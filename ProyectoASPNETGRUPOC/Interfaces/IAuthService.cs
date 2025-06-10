2147bc4924d7c3bdc5192c1e87dc93897b9a6723
Keyner <keyner4899@gmail.com>
2025-06-09 22:26:55 -0300

    Agregue interface IAuthService

---
+++ ProyectoASPNETGRUPOC/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using ProyectoASPNETGRUPOC.Model.DTO;
+using ProyectoASPNETGRUPOC.Model;
+
+namespace ProyectoASPNETGRUPOC.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> LoginUsuario(LoginModel login);
+
+        Task Registrar(DtoUsuario dtoUsuario);
+    }
+}
