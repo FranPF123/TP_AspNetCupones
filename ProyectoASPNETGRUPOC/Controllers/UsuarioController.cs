05ef0db53a10da1c77a1c0f6d20b5baa992338ce
JulietaMereles <julietamereles37@gmail.com>
2025-06-09 23:55:33 -0300

    metodo IdRol e id_Rol

--- ProyectoASPNETGRUPOC/Controllers/UsuarioController.cs
+++ ProyectoASPNETGRUPOC/Controllers/UsuarioController.cs
@@ -61,5 +61,47 @@ namespace ProyectoASPNETGRUPOC.Controllers
 				return BadRequest(ex.Message);
 			}
 		}
-	}
+        [HttpGet("UsuariosSegunRol/{id_Rol}")]
+        [Authorize(Policy = "ControlUsuarios")]
+        public async Task<IActionResult> GetUsuariosPorRol([FromRoute] int id_Rol)
+        {
+            try
+            {
+                List<Usuario> lista = await UServices.GetUsuariosPorRol(id_Rol);
+
+                return Ok(lista);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);//No se encontró el recurso solicitado.
+            }
+        }
+
+        [HttpPost("CrearAdminOAuditor/{IdRol}")]//Solo el admin
+        [Authorize(Policy = "ControlUsuarios")]
+        public async Task<IActionResult> PostAdmin(DtoUsuario dtoUsuario, int IdRol)
+        {
+            try
+            {
+                if (dtoUsuario == null)
+                {
+                    return BadRequest("Datos de usuario mal formados"); //Los datos enviados están mal formados, faltan, o no cumplen una validación.
+                }
+                dtoUsuario.Id_Rol = IdRol; //IdRol = 1 Crear admin, IdRol = 3 Crear auditor: Segun el id de tu tabla roles
+                await UServices.PostCrearAdmin(dtoUsuario);
+
+                return Ok(new
+                {
+                    Mensaje = "el Usuario con el Email: " + dtoUsuario.Email + " fue creado correctamente."
+                });
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return Conflict(ex.Message);//409 == Conflicto de datos, como intentar registrar un email ya existente
+            }
+
+
+        }
+
+    }
 }
