2fc1322055f301804ed7d8bb519746b9055e1c4c
fran <franciscopieri123@gmail.com>
2025-06-09 23:32:28 -0300

    Crear UsuarioController con metodo MiPerfil

---
+++ ProyectoASPNETGRUPOC/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoASPNETGRUPOC.Interfaces;
+
+namespace ProyectoASPNETGRUPOC.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsuarioController : ControllerBase
+	{
+		public readonly IUsuarioServices UServices;
+
+		public UsuarioController(IUsuarioServices uServices)
+		{
+			UServices = uServices;
+		}
+
+		[HttpGet("MiPerfil")]
+		[Authorize]
+		public async Task<IActionResult> miPerfil()
+		{
+			try
+			{
+				var id = User.FindFirst("Id")?.Value;
+				if (id == null) return NotFound("Id NULL");
+				int idUsuario = int.Parse(id);
+				var UsuarioLogeado = await UServices.obtenerDatosDeUsuarioLogeado(idUsuario);
+
+				return Ok(new
+				{
+					Mensaje = "Mi Perfil:",
+
+					usuarioLogeado = UsuarioLogeado
+				});
+			}
+			catch (KeyNotFoundException ex)
+			{
+				return NotFound(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
