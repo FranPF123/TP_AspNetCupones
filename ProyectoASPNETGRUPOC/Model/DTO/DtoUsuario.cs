8bd627ac08a56953eb4da8b8e4307a394151c505
JulietaMereles <julietamereles37@gmail.com>
2025-06-09 22:02:01 -0300

    Agregue carpeta DTO con un DTOUsuario y un DTOUsuarioMuestra

---
+++ ProyectoASPNETGRUPOC/Model/DTO/DtoUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ProyectoASPNETGRUPOC.Model.DTO
+{
+    public class DtoUsuario
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        public string Dni { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [JsonIgnore]
+        public int Id_Rol { get; set; } = 2;
+    }
+}
---
+++ ProyectoASPNETGRUPOC/Model/DTO/DtoUsuarioMuestra.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoASPNETGRUPOC.Model.DTO
+{
+    public class DtoUsuarioMuestra
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Dni { get; set; }
+
+        public string Email { get; set; }
+
+        public string? tipoUsuario { get; set; }
+    }
+}
